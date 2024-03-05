eed2e74123b6dfc262500fa6cdfae10346bc00dc
Gerard Paapu <gerard@gerardpaapu.com>
2024-03-06 12:24:57 +1300

    fix: found some bugs on the first day of korihi

--- client/pages/ProfilePage.tsx
+++ client/pages/ProfilePage.tsx
@@ -3,19 +3,18 @@ import Profile from '../components/Profile.tsx'
 import PostsBy from '../components/PostsBy.tsx'
 import useCredentials from '../hooks/use-auth.ts'
 import { useUserData } from '../hooks/use-userdata.ts'
-
+import { USERNAME } from '../env.ts'
 export default function ProfilePage() {
   const { username } = useParams()
   if (!username) {
     throw new Error()
   }
-  const { username: currentUser } = useCredentials()
   const user = useUserData(username)
 
   return (
     <>
       <Profile username={username} />
-      {user.data && user.data.user_name === currentUser && (
+      {user.data && user.data.user_name === USERNAME && (
         <>
           <h3>Edit your profile data</h3>
           {/* TODO: Implement a form to edit profile data */}
